45a8312092d0a9775a94b731c2515cf79235d267
Marcel Lucas <lucas.marcel@epitech.eu>
2018-02-08 13:12:29 +0100

    Merge

--- src/navy.c
+++ src/navy.c
@@ -7,11 +7,12 @@
 
 # include "navy.h"
 
-void navy(data_t *data)
+int navy(int ac, char **av)
 {
 	my_putstr("my_pid: ");
 	my_put_nbr(data->pid);
 	my_putchar('\n');
 	free_p1(data->p1);
 	free_p2(data->p2);
+	return (data->status);
 }
