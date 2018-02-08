629e997157003e85f626a0216c53407243b7fc9d
Marcel Lucas <lucas.marcel@epitech.eu>
2018-02-08 20:30:51 +0100

    map_add_boat

--- src/map/map_add_boat.c
+++ src/map/map_add_boat.c
@@ -6,24 +6,63 @@
 */
 
 #include "navy.h"
+static char **map_add_cas1(char **map, int nb, int i)
+{
+	int a;
+	int b;
 
-char ** map_add_boat(char **map)
+	a = data->info[i][3] - 48 + 1;
+	b = (data->info[i][2] - 64) * 2;
+	if (data->info[i][3] > data->info[i][6]) {
+		while (nb > 0) {
+			map[a][b] = data->info[i][0];
+			nb--;
+			a -= 1;
+		}
+	} else if (data->info[i][3] < data->info[i][6]) {
+		while (nb > 0) {
+			map[a][b] = data->info[i][0];
+			nb--;
+			a += 1;
+		}
+	}
+	return (map);
+}
+
+static char **map_add_cas2(char **map, int nb, int i)
+{
+	int a;
+	int b;
+
+	a = data->info[i][3] - 48 + 1;
+	b = (data->info[i][2] - 64) * 2;
+	if (data->info[i][2] < data->info[i][5]) {
+		while (nb > 0) {
+			map[a][b] = data->info[i][0];
+			nb--;
+			b += 2;
+		}
+	} else if (data->info[i][2] < data->info[i][5]) {
+		while (nb > 0) {
+			map[a][b] = data->info[i][0];
+			nb--;
+			b -= 2;
+		}
+	}
+	return (map);
+}
+
+char **map_add_boat(char **map)
 {
 	int nb;
 
-	map_affichage();
 	for (int i = 0; data->info[i] != NULL; i++) {
 		nb = data->info[i][0] - 48;
 		if (data->info[i][2] == data->info[i][5]) {
-			while (nb > 1) {
-				data->map[i]
-				nb--;
-			}
+			map = map_add_cas1(map, nb, i);
 		}
 		else if (data->info[i][3] == data->info[i][6]) {
-			while (nb > 1) {
-				nb--;
-			}
+			map = map_add_cas2(map, nb, i);
 		}
 	}
 	return (map);
