4a271951b39f6b178f8227f245d2c53de27be490
Marcel Lucas <lucas.marcel@epitech.eu>
2018-02-08 15:05:24 +0100

    error map management end

--- src/map/check_map_error.c
+++ src/map/check_map_error.c
@@ -7,14 +7,75 @@
 
 #include "navy.h"
 
+static int check_line(char *str)
+{
+	if (str[3] < '1' || str[3] > '8')
+		return (-1);
+	if (str[6] < '1' || str[6] > '8')
+		return (-1);
+	return (0);
+	
+}
+
+static int check_colone(char *str)
+{
+	if (str[2] < 'A' || str[2] > 'H')
+		return (-1);
+	if (str[5] < 'A' || str[5] > 'H')
+		return (-1);
+	return (0);
+}
+
+static int check_info(char *str)
+{
+	int err;
+
+	err = check_line(str);
+	if (err == -1)
+		return (-1);
+	err = check_colone(str);
+	if (err == -1)
+		return (-1);
+	return (0);
+}
+
+static int check_boat(data_t *data)
+{
+	for (int i = 0; data->info[i] != NULL; i++) {
+		if (data->info[i][0] < '2' ||
+		data->info[i][0] > '5') {
+			my_puterr("invalid boats\n", false);
+			return (-1);
+		}
+	}
+	if (data->info[0][0] != data->info[1][0]
+	&& data->info[0][0] != data->info[2][0]
+	&& data->info[0][0] != data->info[3][0]
+	&& data->info[1][0] != data->info[2][0]
+	&& data->info[1][0] != data->info[3][0]
+	&& data->info[2][0] != data->info[3][0]) {
+		return (0);
+	}	
+	my_puterr("You need 4 different boats !!\n", false);
+	return (-1);
+}
+
 int check_map_error(data_t *data, char *buff)
 {
-	if (my_strlen(buff) < 31) {
-		my_puterr("map invalid !!", false);
+	int err;
+
+	if (my_strlen(buff) < 31)
 		return (-1);
-	}
 	data->info = my_strtok(buff, '\n');
 	for (int i = 0; data->info[i] != NULL; i++)
 		printf("A%s\n", data->info[i]);
+	err = check_boat(data);
+	if (err == -1)
+		return (-1);
+	for (int i = 0; data->info[i] != NULL; i++) {
+		err = check_info(data->info[i]);
+		if (err == -1)
+			return (-1);
+	}
 	return (0);
 }
