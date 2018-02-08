4fe1d921acccdd6fd6d10a7b71dd3c1d39cdf525
Cyril COLINET <cyril.colinet@epitech.eu>
2018-02-08 13:10:54 +0100

    Ading connector

---
+++ src/connector/connector.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2018
+** navy
+** File description:
+** connector functions
+*/
+
+# include "navy.h"
+
+bool connector(data_t *data)
+{
+	if (data->type == playerOne) {
+		my_putstr("waiting for enemy connection...\n");
+	} else {
+
+	}
+
+	return (true);
+}
