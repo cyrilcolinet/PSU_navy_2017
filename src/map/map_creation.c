8645e9fcb743ac84e99fadbd4aa27b086efe1f3d
Marcel Lucas <lucas.marcel@epitech.eu>
2018-02-08 15:30:25 +0100

    map creation / malloc

--- src/map/map_creation.c
+++ src/map/map_creation.c
@@ -7,7 +7,23 @@
 
 #include "navy.h"
 
+static void map_renseignement(char **map)
+{
+	for (int i = 0; map[i] != NULL; i++) {
+		map[i] = malloc(sizeof(char) * 18);
+		map[i] = '\0';
+	}
+}
+
 void map_creation(data_t *data)
 {
-	if (data->type = playerOne);
+	if (data->type == playerOne) {
+		data->p1->map = malloc(sizeof(char*) * 11);
+		data->p1->map[10];
+		map_renseignement(data->p1->map);
+	} else if (data->type == playerOne) {
+		data->p2->map = malloc(sizeof(char*) * 11);
+		data->p2->map[10];
+		map_renseignement(data->p2->map);
+	}
 }
