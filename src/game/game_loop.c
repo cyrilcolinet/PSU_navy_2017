aa0317b23598eedb80947b66411cc40615f49958
Marcel Lucas <lucas.marcel@epitech.eu>
2018-02-12 16:53:43 +0100

    loop game management modif

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -11,20 +11,30 @@ void player_turn(void)
 {
 	while (1) {
 		map_display();
+		str = get_next_line;
+		send_data(str);
+		get_sended_response();
+		pause();
+		if (data->received) {
+			check_hit_fail(data->data, data->enemy, str);
+			data->received = false;
+		}
 		if (check_end_game(data->map) == 1) {
-			my_putstr("Enemy won\n");
-			data->status = 1;
+			my_putstr("I won\n");
+			data->status = 0;
 			break;
 		}
+		my_putstr("waiting for enemy's attack...\n");
 		get_sended_data();
 		pause();
 		if (data->received) {
-			check_hit_fail(data->data, data->map);
+			check_hit_fail(data->data, data->map, str);
 			data->received = false;
 		}
+		send_response();
 		if (check_end_game(data->enemy) == 1) {
-			my_putstr("I won\n");
-			data->status = 0;
+			my_putstr("Enemy won\n");
+			data->status = 1;
 			break;
 		}
 	}
