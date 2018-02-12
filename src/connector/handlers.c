ec71d19d396c091420fae40c4072e3bbcc463026
Cyril COLINET <cyril.colinet@epitech.eu>
2018-02-12 19:25:35 +0100

    debig

--- src/connector/handlers.c
+++ src/connector/handlers.c
@@ -26,6 +26,7 @@ void data_handler(int sig, siginfo_t *si, void *ptr)
 		data->data++;
 	} else if (si->si_signo == SIGUSR2) {
 		data->data++;
+		printf(" => GET (data): %d\n", data->data);
 		data->received = true;
 	}
 }
@@ -39,6 +40,7 @@ void response_handler(int sig, siginfo_t *si, void *ptr)
 		data->data++;
 	} else if (si->si_signo == SIGUSR2) {
 		data->data++;
+		printf(" => GET (response): %d\n", data->data);
 		data->received = true;
 	}
 }
