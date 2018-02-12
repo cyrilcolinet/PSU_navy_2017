d7da9a2845fffc068c567947364125643b79708f
Marcel Lucas <lucas.marcel@epitech.eu>
2018-02-12 16:20:55 +0100

    loop game condition

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -9,13 +9,21 @@
 
 void player_turn(void)
 {
-	while (data->status == 0) {
+	while (1) {
+		if (check_end_game(data->map) == 1) {
+			my_putstr("Enemy won\n");
+			break;
+		}
 		get_sended_data();
 		pause();
 
 		if (data->received) {
 			data->received = false;
 			map_display();
+		}
+		if (check_end_game(data->enemy) == 1) {
+			my_putstr("I won\n");
+			break;
 		}
 	}
 }
