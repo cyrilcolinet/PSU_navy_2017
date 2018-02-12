8c03e4c52a3c2eda42dc6976a1f2d9bdb6d77110
Cyril COLINET <cyril.colinet@epitech.eu>
2018-02-12 20:14:32 +0100

    Fix problems

--- src/connector/connector.c
+++ src/connector/connector.c
@@ -34,6 +34,7 @@ bool send_response(int resp)
 		return (false);
 	}
 
+	printf("\n => SEND (response) %d to %d\n", resp, data->pid2);
 	for (loop = 0; loop < resp; loop++) {
 		if (!send_signal(data->pid2, SIGUSR1)) {
 			write(2, "Unable to send signal to receiver.\n", 35);
@@ -53,6 +54,8 @@ bool send_data(char *column)
 {
 	int bit = get_case_number(column);
 	int loop;
+
+	printf("\n => SEND (data) %d to %d\n", bit, data->pid2);
 
 	for (loop = 0; loop < bit; loop++) {
 		if (!send_signal(data->pid2, SIGUSR1)) {
